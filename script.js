b16de1c9e5816bd22d0f83d6df736714c0b8077f
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-03-13 14:18:21 +0530

    Download as PDF and bug fixes done

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-var newImg, showImg
+var newImg, showImg, filename
 
-loadFile = (e) => {
+function loadFile(e) {
   showImg = document.getElementById('showImg')
   showImg.src = URL.createObjectURL(e.target.files[0])
 
   newImg = document.createElement('img')
-  newImg.src = URL.createObjectURL(e.target.fles[0])
+  newImg.src = URL.createObjectURL(e.target.files[0])
 
+  filename = e.target.files[0].name
   showImg.onload = function () {
     URL.revokeObjectURL(showImg.src)
   }
+}
+
+function pdfDown() {
+  console.log(newImg)
+  var doc = new jsPDF()
+  doc.addImage(newImg, 10, 10)
+  doc.save(`${filename}.pdf`)
 }
